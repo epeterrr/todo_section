afe02939a5da2a3022003ca7d48d40ef55e49fdf
Excel <eime_peter@gmail.com>
2023-07-07 20:42:30 +0000

    todo

--- app/src/main/java/com/example/first/MainActivity.java
+++ app/src/main/java/com/example/first/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.first;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setOnClickListener(v -> {
+            // Perform your desired action here when imageView is clicked
+        });
+
+        ImageView imageView2 = findViewById(R.id.imageView2);
+        imageView2.setOnClickListener(v -> {
+            // Perform your desired action here when imageView2 is clicked
+        });
     }
-
-
-
 }
